68fcc44a0f27136272c5ec2ce946042f97aaee72
CapitaineThug <nicolasschmid2007@gmail.com>
2024-08-31 09:54:29 +0200

    UI Side Menu

--- src/app/workers/Worker.java
+++ src/app/workers/Worker.java
@@ -1,9 +1,22 @@
 package app.workers;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Worker implements WorkerItf {
 
-    //Constructeur
-    public Worker(){
-        
+    // Attributs
+    private ArrayList<File> listPhotos;
+
+    // Constructeur
+    public Worker() {
+        this.listPhotos = new ArrayList<>();
+
+    }
+
+    @Override
+    public List<File> listDirectoryFiles(String path) {
+        return null;
     }
 }
--- src/app/workers/WorkerItf.java
+++ src/app/workers/WorkerItf.java
@@ -1,10 +1,15 @@
 package app.workers;
 
+import java.io.File;
+import java.util.List;
+
 /**
  * Cette interface définit les services "métier" de l'application.
  *
  * @author ...
  */
 public interface WorkerItf {
+
+    List<File> listDirectoryFiles(String path);    
     
 }
